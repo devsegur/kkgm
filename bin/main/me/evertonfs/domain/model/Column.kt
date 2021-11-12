9925f28860d495f225ea43d7837cc638d5f74f98
devsegur <evertonsegur5@gmail.com>
2021-11-12 17:55:46 -0300

    removed bin

--- bin/main/me/evertonfs/domain/model/Column.kt
+++
@@ -1,3 +0,0 @@
-package me.evertonfs.domain.model
-
-data class Column(var id: Long?, var name: String?, var table: Table?)
--- bin/main/me/evertonfs/domain/model/Table.kt
+++
@@ -1,3 +0,0 @@
-package me.evertonfs.domain.model
-
-data class Table( val id: Long?, val name: String?, val reference: List<Column>?)
